17e93ee0cbb0b4053c111546d05ebae01b51144d
KRB001 <kieranbentley0@gmail.com>
2025-02-18 19:07:12 -0500

    small fixes and changes to toggle

--- scripts/toggle-citations.js
+++ scripts/toggle-citations.js
@@ -10,9 +10,13 @@ function toggleCitations() {
     if (state === "none"){
         state = "initial"
         citationButton.style.color = "red"
+        citationButton.style.border = "dotted white 2px"
+        citationButton.innerText = "[OFF]"
     }
     else {
         state = "none"
         citationButton.style.color = "yellow"
+        citationButton.style.border = "solid white 2px"
+        citationButton.innerText = "[ON]"
     }
 }
